b268a65382a572ec848c3b349fb792379e2de2f9
Jithu <jithukrishnaju@gmail.com>
2026-05-03 13:03:13 +0530

    added splash screen.

--- app/src/main/java/com/retailcloudandroid/retailcloudmachinetestandroid/MainActivity.kt
+++ app/src/main/java/com/retailcloudandroid/retailcloudmachinetestandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.retailcloudandroid.retailcloudmachinetestandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             RetailCloudMachineTestAndroidTheme {
